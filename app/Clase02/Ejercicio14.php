1c975f53f1f14ee155e01ef1a5ee40d15488638f
avillegas <correodebitter@gmail.com>
2021-03-24 19:35:57 -0300

    git commit -m "TEST"

--- app/Clase02/Ejercicio14.php
+++ app/Clase02/Ejercicio14.php
@@ -1,4 +1,5 @@
 <?php
+
 /******************************************************************************
 
 Alvaro Villegas
@@ -7,28 +8,30 @@ Aplicación No 14 (Par e impar)
 Crear una función llamada esPar que reciba un valor entero como parámetro y devuelva TRUE
 si este número es par ó FALSE si es impar.
 Reutilizando el código anterior, crear la función esImpar.
-*******************************************************************************/
+ *******************************************************************************/
 
-echo esPar(10);echo "<br/>";
-echo esImpar(11);"<br/>";
+echo esPar(10);
+echo "<br/>";
+echo esImpar(11);
+"<br/>";
 
-echo esImpar(10);echo "<br/>";
+echo esImpar(10);
+echo "<br/>";
 echo esPar(11);
-
+echo "<br/>";
+echo "HUh?";
 
 function esPar(int $numero)
 {
-    if($numero%2 == 0)
-    return true;
-  
-    return false;
 
+    if ($numero % 2 == 0)
+        return true;
+
+    return false;
 }
 
-function esImpar( int $numero)
+function esImpar(int $numero)
 {
+
     return !(esPar($numero));
 }
-
-?>
-
